e03d4f9effddc3a541cc5e4db80b54c7f6e49de4
Hackeranuj <anujgahra244304@gmai.com>
2024-06-30 16:45:07 +0530

    URL- Shortner

---
+++ middlewares/auth.js
@@ -0,0 +1,28 @@
+const { getUser } = require("../service/auth");
+
+async function restrictToLoggedinUserOnly(req, res, next) {
+  const userUid = req.cookies?.uid;
+
+  if (!userUid) return res.redirect("/login");
+  const token = userUid.split("Bearer")[1];
+  const user = getUser(token);
+
+  if (!user) return res.redirect("/login");
+
+  req.user = user;
+  next();
+}
+
+async function checkAuth(req, res, next) {
+  const userUid = req.cookies?.uid;
+
+  const user = getUser(userUid);
+
+  req.user = user;
+  next();
+}
+
+module.exports = {
+  restrictToLoggedinUserOnly,
+  checkAuth,
+};
